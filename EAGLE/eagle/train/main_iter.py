b70a8a17f072da5394c3696eefa6b14bf44fec0d
Langlin Huang <935313841@qq.com>
2025-01-19 15:26:16 -0600

    update

--- EAGLE/eagle/train/main_iter.py
+++ EAGLE/eagle/train/main_iter.py
@@ -354,7 +354,7 @@ for epoch in range(num_epochs + 1):
             optimizer.zero_grad()
             loss = 0
             previous_hs = data["hidden_states"]
-            for _ in range(3):
+            for _ in range(5):
                 predict = model(previous_hs, input_ids=data["input_ids"], attention_mask=data["attention_mask"])
                 last_hs = predict.clone().detach()[:,:-1,:]
                 previous_hs = torch.cat([data["hidden_states"][:,:1,:], last_hs], dim=1)
