9ed5d00a461fa3d4faed6df693d2bbcf387151fd
shrango <935313841@qq.com>
2025-02-04 15:24:55 -0600

    update

--- HASS/evaluation/acceptance_length.py
+++ HASS/evaluation/acceptance_length.py
@@ -15,23 +15,27 @@ avg_accept_length = 0
 def groupby_accept_length(data):
     res = {}
     for d in data:
-        if d['accept_length'] not in res:
-            res[d['accept_length']] = 0
-        res[d['accept_length']] += 1
+        if d not in res:
+            res[d] = 0
+        res[d] += 1
     return res
 
 group_all = {}
+sum_accept_length = 0
 for line in lines:
     data = json.loads(line)
-    subgroup = groupby_accept_length(data['choices'])
+    subgroup = groupby_accept_length(data['choices'][0]['accept_length'])
     for k, v in subgroup.items():
         if k not in group_all:
             group_all[k] = 0
         group_all[k] += v
-    avg_accept_length += sum(data['choices'][0]['accept_length']) / len(data['choices'][0]['accept_length']) + 1
-    compare_avg = sum([i*subgroup[i] for i in subgroup]) / sum(subgroup.values())
+    print(subgroup)
+    avg_accept_length = sum(data['choices'][0]['accept_length']) / len(data['choices'][0]['accept_length']) + 1
+    sum_accept_length += avg_accept_length
+    compare_avg = sum([i*subgroup[i] for i in subgroup]) / sum(subgroup.values()) + 1
+    # print(f"avg_accept_length: {avg_accept_length}, compare_avg: {compare_avg}")
     assert abs(avg_accept_length - compare_avg) < 1e-5
 
-avg_accept_length /= len(lines)
+avg_accept_length = sum_accept_length/len(lines)
 
 print(f"acceptance length:{avg_accept_length}, group_all: {group_all}")
