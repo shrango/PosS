afde99db4ed48decaff6957a6f53918e122e5758
shrango <935313841@qq.com>
2025-02-04 14:36:08 -0600

    update accept

--- HASS/evaluation/acceptance_length.py
+++ HASS/evaluation/acceptance_length.py
@@ -12,10 +12,26 @@ print('num of samples:', len(lines))
 
 avg_accept_length = 0
 
+def groupby_accept_length(data):
+    res = {}
+    for d in data:
+        if d['accept_length'] not in res:
+            res[d['accept_length']] = 0
+        res[d['accept_length']] += 1
+    return res
+
+group_all = {}
 for line in lines:
     data = json.loads(line)
+    subgroup = groupby_accept_length(data['choices'])
+    for k, v in subgroup.items():
+        if k not in group_all:
+            group_all[k] = 0
+        group_all[k] += v
     avg_accept_length += sum(data['choices'][0]['accept_length']) / len(data['choices'][0]['accept_length']) + 1
+    compare_avg = sum([i*subgroup[i] for i in subgroup]) / sum(subgroup.values())
+    assert abs(avg_accept_length - compare_avg) < 1e-5
 
 avg_accept_length /= len(lines)
 
-print("acceptance length:", avg_accept_length)
+print(f"acceptance length:{avg_accept_length}, group_all: {group_all}")
