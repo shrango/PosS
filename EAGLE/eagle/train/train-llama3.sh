01ca2a51eb07d6fcef61411a5fe98dfa5eb0180b
langlin <h.langlin@wustl.edu>
2025-02-15 22:23:55 +0000

    fuck

--- EAGLE/eagle/train/train-llama3.sh
+++
@@ -1,6 +0,0 @@
-ckpt=/home/jovyan/workspace/project/llama3/standard/checkpoint
-mkdir -p $ckpt
-prep=/home/jovyan/workspace/preprocess/sharegpt_0_67999_mufp16
-
-accelerate launch -m --mixed_precision=bf16 eagle.train.main --tmpdir $prep \
---cpdir $ckpt --configpath EAGLE-LLaMA3-Instruct-8B
