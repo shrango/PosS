33a7200de67260f8202ffc711bdcec024b1093db
Langlin Huang <935313841@qq.com>
2025-03-08 03:38:18 -0600

    Update cnets_agent_layer_gen.py

--- HASS/model/cnets_agent_layer_gen.py
+++ HASS/model/cnets_agent_layer_gen.py
@@ -723,10 +723,12 @@ class Model(nn.Module):
             if hasattr(self, "stable_kv") and self.stable_kv[current_layer] is not None:
                 kv_len = self.stable_kv[current_layer][0][0].shape[2]
                 # pdb.set_trace()
-                # out_hidden, past_key_values, position_ids = self(mid_hidden_states, input_ids=input_ids[:, kv_len:],
-                                            #    past_key_values=self.stable_kv[current_layer], position_ids=position_ids, use_cache=True, forward_layer=current_layer)
-                out_hidden, past_key_values, _ = self(mid_hidden_states[:,kv_len:,:], input_ids=input_ids[:, kv_len:],
-                                               past_key_values=self.stable_kv[current_layer], position_ids=position_ids[kv_len:], use_cache=True, forward_layer=current_layer)
+                if mid_hidden_states.shape[1] < kv_len:
+                    out_hidden, past_key_values, position_ids = self(mid_hidden_states, input_ids=input_ids[:, kv_len:],
+                                                past_key_values=self.stable_kv[current_layer], position_ids=position_ids, use_cache=True, forward_layer=current_layer)
+                else:
+                    out_hidden, past_key_values, _ = self(mid_hidden_states[:,kv_len:,:], input_ids=input_ids[:, kv_len:],
+                                                past_key_values=self.stable_kv[current_layer], position_ids=position_ids[kv_len:], use_cache=True, forward_layer=current_layer)
             else:
                 # out_hidden, past_key_values = self(input_hidden, input_ids=input_ids, past_key_values=past_key_values,
                                             #    position_ids=position_ids, use_cache=True, forward_num=current_layer)
