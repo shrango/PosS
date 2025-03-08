90675adbf634219caf8bda94fadfc7d61abfe0a8
Langlin Huang <935313841@qq.com>
2025-03-08 03:25:09 -0600

    Update cnets_agent_layer_gen.py

--- HASS/model/cnets_agent_layer_gen.py
+++ HASS/model/cnets_agent_layer_gen.py
@@ -715,7 +715,7 @@ class Model(nn.Module):
             current_layer = (1 + i)//self.position_per_layer
             self.tree_mask = tree_mask
             new_position_ids = len_posi + self.position_ids
-            position_ids = torch.cat((position_ids.squeeze(0), new_position_ids), dim=0)
+            position_ids = torch.cat((position_ids, new_position_ids), dim=0)
             
             # with Timer("draft one"):
             pdb.set_trace()
@@ -724,12 +724,12 @@ class Model(nn.Module):
                 # pdb.set_trace()
                 # out_hidden, past_key_values, position_ids = self(mid_hidden_states, input_ids=input_ids[:, kv_len:],
                                             #    past_key_values=self.stable_kv[current_layer], position_ids=position_ids, use_cache=True, forward_layer=current_layer)
-                out_hidden, past_key_values, position_ids = self(mid_hidden_states[:,kv_len:,:], input_ids=input_ids[:, kv_len:],
+                out_hidden, past_key_values, _ = self(mid_hidden_states[:,kv_len:,:], input_ids=input_ids[:, kv_len:],
                                                past_key_values=self.stable_kv[current_layer], position_ids=position_ids[kv_len:], use_cache=True, forward_layer=current_layer)
             else:
                 # out_hidden, past_key_values = self(input_hidden, input_ids=input_ids, past_key_values=past_key_values,
                                             #    position_ids=position_ids, use_cache=True, forward_num=current_layer)
-                out_hidden, past_key_values, position_ids = self(mid_hidden_states, input_ids=input_ids, position_ids=position_ids, use_cache=True, forward_layer=current_layer)
+                out_hidden, past_key_values, _ = self(mid_hidden_states, input_ids=input_ids, position_ids=position_ids, use_cache=True, forward_layer=current_layer)
             
             out_hidden = out_hidden[:, -top_k:]
             self.stable_kv[current_layer] = ((past_key_values[0][0][:,:,:self.stable_kv[0][0][0].shape[2],:], past_key_values[0][1][:,:,:self.stable_kv[0][0][1].shape[2],:]),)
