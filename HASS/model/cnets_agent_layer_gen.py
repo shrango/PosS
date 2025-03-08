020a8323567d38b9e7b6db5967817e9e2c5f76d9
Langlin Huang <935313841@qq.com>
2025-03-08 05:15:32 -0600

    Update cnets_agent_layer_gen.py

--- HASS/model/cnets_agent_layer_gen.py
+++ HASS/model/cnets_agent_layer_gen.py
@@ -738,8 +738,9 @@ class Model(nn.Module):
                 if first_round==True and i%self.position_per_layer!=0:
                     out_hidden, past_key_values, _ = self(input_hidden, input_ids=new_token, past_key_values=past_key_values,
                                                position_ids=position_ids, use_cache=True)
-                out_hidden, past_key_values, position_ids = self(mid_hidden_states, input_ids=input_ids[:, kv_len:],
-                                               past_key_values=self.stable_kv[current_layer], position_ids=position_ids, use_cache=True, forward_layer=current_layer)
+                else:
+                    out_hidden, past_key_values, position_ids = self(mid_hidden_states, input_ids=input_ids[:, kv_len:],
+                                                past_key_values=self.stable_kv[current_layer], position_ids=position_ids, use_cache=True, forward_layer=current_layer)
             else:
                 # out_hidden, past_key_values = self(input_hidden, input_ids=input_ids, past_key_values=past_key_values,
                                             #    position_ids=position_ids, use_cache=True, forward_num=current_layer)
